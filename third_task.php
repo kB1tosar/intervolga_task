0dbd4b2dc55a34401a1ef44c4e786e52b0e164c6
tosarL-V1 <34838236+tosarL-V1@users.noreply.github.com>
2020-01-21 18:33:44 +0400

    Start

--- third_task.php
+++ third_task.php
@@ -14,14 +14,14 @@ function seacrh_of_repeat_number($array)
 
 $array = [];
 
-for ($i=0;$i<100;$i++){
+for ($i = 0; $i < 100; $i++) {
     array_push($array, rand(1, 10));
 }
 
 $count_of_all_double_number = seacrh_of_repeat_number($array);
 
-foreach ($array as $value){
-    echo $value.", ";
+foreach ($array as $value) {
+    echo $value . ", ";
 }
-echo ('<br>');
+echo('<br>');
 echo $count_of_all_double_number;
