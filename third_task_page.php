4211107dcda219bc573d4f1841cef5337bc11484
tosarL-V1 <34838236+tosarL-V1@users.noreply.github.com>
2020-02-10 15:51:37 +0400

    third task

--- third_task_page.php
+++ third_task_page.php
@@ -4,20 +4,23 @@
 <head>
     <meta charset="UTF-8">
     <? include('front.php'); ?>
+    <title>Третье задание</title>
 </head>
 <body>
 <? include('header.php'); ?>
 <div class="indents_body">
-    <h1>Задание третье</h1>
+    <h1>Третье задание </h1>
     <h2>Массивы</h2>
     <p>Дан массив из 100 элементов. Требуется вывести количество
         последовательных пар одинаковых элементов<br>
         Например:<br>
         1, 1, 2, 3, 4, -51, 12, 12, 12, -51<br>
         Для примера ответ "3"<br>
-        пришлите ссылку на репозиторий с решением<br></p>
-    <p>Массив:<br><? foreach ($array as $value) echo $value . ", "; ?> </p>
-    <p>Ответ:<br><? echo $count_of_all_double_number; ?></p>
+        пришлите ссылку на репозиторий с решением<br></p><hr>
+    <h3>Массив:</h3>
+    <p><? foreach ($array as $value) echo $value . ", "; ?> </p>
+    <h3>Ответ:</h3>
+    <p><? echo $count_of_all_double_number; ?></p>
 </div>
 <? include('footer.php'); ?>
 </body>
