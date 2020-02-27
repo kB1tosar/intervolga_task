c6d4cf3c5aaef0c3fda2c50fe55f4215f2c84dfd
tosarL-V1 <34838236+tosarL-V1@users.noreply.github.com>
2020-02-27 20:17:45 +0400

    Title

--- header.php
+++ header.php
@@ -1,33 +1,33 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="ru">
 <head>
     <meta charset="UTF-8">
-    <? include('front.php'); ?>
+    <? include('front.php');
+   include_once('json.php');
+   ?>
 </head>
 <body>
-<div id="header_id">
-    <header class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
-        <div class="container">
-            <a class="navbar-brand" href="index.php">Задание для стажёра</a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarNav">
-                <ul class="navbar-nav">
-                    <li class="nav-item">
-                        <a class="nav-link" href="first_task_page.php">Первое задание <span class="sr-only">(current)</span></a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="second_task_page.php">Второе задание <span class="sr-only">(current)</span></a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="third_task_page.php">Третье задание</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="https://volgograd.hh.ru/resume/f00166caff044bc7be0039ed1f414668363034">Ссылка на hh</a>
-                    </li>
-                </ul>
-            </div>
+<header class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+    <div class="container">
+        <a class="navbar-brand" href="index.php">Задание для стажёра</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link" href="first_task_page.php">Первое задание <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="second_task_page.php">Второе задание <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="third_task_page.php">Третье задание</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="https://volgograd.hh.ru/resume/f00166caff044bc7be0039ed1f414668363034">Ссылка на hh</a>
+                </li>
+            </ul>
         </div>
-    </header>
-</div>
+    </div>
+</header>
---
+++ json.php
@@ -0,0 +1,8 @@
+<?php
+$title_json_file = file_get_contents('includes/title.json');
+$title_list = json_decode($title_json_file,true);
+foreach ($title_list['data_title'] as $title):
+    if ($title['path'] === $_SERVER['SCRIPT_NAME']):?>
+        <title><?=$title['title']?></title>
+    <?endif;?>
+<?endforeach;?>
