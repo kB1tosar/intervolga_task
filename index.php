07adcbabe09277995507b858f6e1ab51cff69923
tosarL-V1 <34838236+tosarL-V1@users.noreply.github.com>
2020-02-27 20:13:39 +0400

    New main page

--- index.php
+++ index.php
@@ -1,6 +1,8 @@
-<title>Главная</title>
 <? include('header.php'); ?>
 <div class="container mt-5 mb-5 pt-3 pb-3">
-    <h1>hello world</h1>
+    <h1>Это задание сделано для компании Intervolga</h1>
+    <p>Если Вам понравилось, пожалуйста позовите на собеседование</p>
+    <p><del>А ещё лучше примите меня работать к себе</del></p>
+    <img src="futurama-transparent-frog-2.gif" class="img-thumbnail rounded mx-auto d-block">
 </div>
 <? include('footer.php'); ?>
